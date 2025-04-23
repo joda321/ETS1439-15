71bd33085c47a8865e03e6854ae8c3f7943f51cd
Yohannes <yohannesdamtew25@gmail.com>
2025-04-23 11:51:06 -0700

    string_method

---
+++ Python/str_replace.py
@@ -0,0 +1,8 @@
+# str_replace.py
+
+text = "Python is fun. Python is powerful."
+print("Original:", text)
+
+# Replace 'Python' with 'Coding'
+new_text = text.replace("Python", "Coding")
+print("Modified:", new_text)
