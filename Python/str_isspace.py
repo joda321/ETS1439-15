3b39fc157b4cc5344b46a9e9ad665780f74fa62b
Yohannes <yohannesdamtew25@gmail.com>
2025-05-01 23:13:04 -0700

    string_method

---
+++ Python/str_isspace.py
@@ -0,0 +1,12 @@
+# Example: Checking for empty or whitespace-only lines in text
+
+lines = ["Hello", "   ", "\t", "World", "", "\n"]
+print("Lines with only whitespace:")
+
+for i, line in enumerate(lines):
+    if line.isspace():
+        print("Line {} is only whitespace.".format(i))
+    elif line == "":
+        print("Line {} is empty.".format(i))
+    else:
+        print("Line {} has content: '{}'".format(i, line))
